83e75a642b3462ac355974bec99c3073aa1e778c
Ildomar Antono <ildomar01@hotmail.com>
2024-06-11 23:11:57 -0300

    projeto

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
 function adicionar() {
-    const produtoSelect = document.getElementById('produto');
-    const quantidadeInput = document.getElementById('quantidade');
-    const listaProdutos = document.getElementById('lista-produtos');
-    const valorTotalElement = document.getElementById('valor-total');
+    let produtoSelect = document.getElementById('produto');
+    let quantidadeInput = document.getElementById('quantidade');
+    let listaProdutos = document.getElementById('lista-produtos');
+    let valorTotalElement = document.getElementById('valor-total');
   
-    const produtoInfo = produtoSelect.value.split(' - ');
-    const produtoNome = produtoInfo[0];
-    const produtoPreco = parseFloat(produtoInfo[1].replace('R$', ''));
-    const quantidade = parseInt(quantidadeInput.value);
+    let produtoInfo = produtoSelect.value.split(' - ');
+    let produtoNome = produtoInfo[0];
+    let produtoPreco = parseFloat(produtoInfo[1].replace('R$', ''));
+    let quantidade = parseInt(quantidadeInput.value);
   
     if (isNaN(quantidade) || quantidade <= 0) {
       alert('Por favor, insira uma quantidade válida.');
       return;
     }
   
-    const produtoTotal = produtoPreco * quantidade;
+    let produtoTotal = produtoPreco * quantidade;
   
-    const novoProduto = document.createElement('section');
+    let novoProduto = document.createElement('section');
     novoProduto.classList.add('carrinho__produtos__produto');
     novoProduto.innerHTML = `<span class="texto-azul">${quantidade}x</span> ${produtoNome} <span class="texto-azul">R$${produtoTotal.toFixed(2)}</span>`;
   
     listaProdutos.appendChild(novoProduto);
   
-    const valorTotalAtual = parseFloat(valorTotalElement.innerText.replace('R$', ''));
-    const novoValorTotal = valorTotalAtual + produtoTotal;
+    let valorTotalAtual = parseFloat(valorTotalElement.innerText.replace('R$', ''));
+    let novoValorTotal = valorTotalAtual + produtoTotal;
   
     valorTotalElement.innerText = `R$${novoValorTotal.toFixed(2)}`;
   
@@ -31,8 +31,8 @@ function adicionar() {
   }
   
   function limpar() {
-    const listaProdutos = document.getElementById('lista-produtos');
-    const valorTotalElement = document.getElementById('valor-total');
+    let listaProdutos = document.getElementById('lista-produtos');
+    let valorTotalElement = document.getElementById('valor-total');
   
     while (listaProdutos.firstChild) {
       listaProdutos.removeChild(listaProdutos.firstChild);
